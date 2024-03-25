cf33dfae96176d776697fb2230339e4f15f79a33
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2024-03-25 08:26:20 +0100

    added main components and basic structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './shared/menu/menu.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LogInComponent } from './log-in/log-in.component';
+import { SummaryComponent } from './main/summary/summary.component';
+import { AddTaskComponent } from './main/add-task/add-task.component';
+import { ContactsComponent } from './main/contacts/contacts.component';
+import { BoardComponent } from './main/board/board.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LogInComponent },
+    { path: 'summary', component: SummaryComponent },
+    { path: 'add_task', component: AddTaskComponent },
+    { path: 'board', component: BoardComponent },
+    { path: 'contacts', component: ContactsComponent }
+];
