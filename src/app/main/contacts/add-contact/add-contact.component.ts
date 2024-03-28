15e318b1e2a173be4bbd95872cf85a186f4d1b96
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2024-03-28 16:21:59 +0100

    overlay basic structure

--- src/app/main/contacts/add-contact/add-contact.component.ts
+++ src/app/main/contacts/add-contact/add-contact.component.ts
@@ -1,9 +1,10 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { ContactIconComponent } from '../contact-list-item/contact-icon/contact-icon.component';
 
 @Component({
   selector: 'app-add-contact',
   standalone: true,
-  imports: [],
+  imports: [ContactIconComponent],
   templateUrl: './add-contact.component.html',
   styleUrl: './add-contact.component.scss'
 })
