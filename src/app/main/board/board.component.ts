9fe779009375a34bc30402435d663bb6096561f1
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2024-04-02 15:24:32 +0200

    updating despite erroneous behavior

--- src/app/main/board/board.component.ts
+++ src/app/main/board/board.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Task } from '../../../models/task';
 import { TaskCardComponent } from './task-card/task-card.component';
 import { MainService } from '../../shared/main.service';
@@ -11,16 +11,32 @@ import { MainService } from '../../shared/main.service';
   styleUrl: './board.component.scss'
 })
 export class BoardComponent {
-  tasks: Task[] = [];
+  tasks: Task[];
+  toDo: Task[];
+  inProgress: Task[];
+  awaitFeedback: Task[];
+  done: Task[];
 
   constructor(private mainService: MainService ) {
-    mainService.tasksChanged$.subscribe((tasks: Task[]) => {this.tasks = tasks});
+    this.tasks = [];
+    this.toDo = [];
+    this.inProgress = [];
+    this.awaitFeedback = [];
+    this.done = [];
   }
 
-  filterTasksBy(status: 'To do' | 'In progress' | 'Await feedback' | 'Done'): Task[] {
-    if(this.tasks.filter(t => t.status == status).length > 0) {
-      console.log(this.tasks.filter(t => t.status == status));
-    }
-    return this.tasks.filter(t => t.status == status);
+  ngOnInit() {
+    this.mainService.tasksChanged$.subscribe((tasks: Task[]) => {
+      this.tasks = tasks;
+      this.filterTasks();
+      console.log(this.toDo.length);
+    });
+  }
+
+  filterTasks(): void {
+    this.toDo = this.tasks.filter(t => t.status == 'To do');
+    this.inProgress = this.tasks.filter(t => t.status == 'In progress');
+    this.awaitFeedback = this.tasks.filter(t => t.status == 'Await feedback');
+    this.done = this.tasks.filter(t => t.status == 'Done');
   }
 }
