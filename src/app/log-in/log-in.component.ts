f18166239c4901f16873f2d3e1199846f03b7bdc
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2024-04-08 13:44:02 +0200

    merged forms

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,22 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-log-in',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.scss'
 })
 export class LogInComponent {
   animate: boolean = true; 
-  form: 'log in' | 'sign up' = 'log in';
+  form: 'Log in' | 'Sign up' = 'Log in';
   rememberLogIn: boolean = false;
   acceptPrivacyPolicy: boolean = false;
 
   toggleForm() {
     this.animate = false;
-    this.form == 'log in' ? this.form = 'sign up' : this.form = 'log in';
+    this.form == 'Log in' ? this.form = 'Sign up' : this.form = 'Log in';
   }
 
   toggleRememberMe() {
