bf56b725d85b44843c7300e1b0f2644bc9b4c2ee
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2024-04-08 10:59:40 +0200

    created login form

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-log-in',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.scss'
 })
 export class LogInComponent {
+form: 'log in' | 'sign up' = 'log in';
+rememberLogIn: boolean = false;
 
+toggleRememberMe() {
+  this.rememberLogIn = !this.rememberLogIn;
+}
 }
