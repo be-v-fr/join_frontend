f0978b1e23d696d01cb7551a2f166db5c3f716f7
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2024-04-17 09:18:19 +0200

    add contact overlay responsive complete

--- src/app/templates/close-btn/close-btn.component.ts
+++ src/app/templates/close-btn/close-btn.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-close-btn',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './close-btn.component.html',
   styleUrl: './close-btn.component.scss'
 })
 export class CloseBtnComponent {
-
+@Input() bright?: boolean;
 }
