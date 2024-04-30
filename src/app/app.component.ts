ec6c2c0417a51fa40e96a7a91a734b30886b6912
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2024-04-30 16:12:05 +0200

    added app component documentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,10 @@ import { UsersService } from './services/users.service';
 import { AuthService } from './services/auth.service';
 import { Subscription } from 'rxjs';
 
+
+/**
+ * This is the app component created by Angular by default (but extended in many ways).
+ */
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -19,27 +23,43 @@ export class AppComponent implements OnDestroy {
   title = 'join';
   private authService = inject(AuthService);
   private usersService = inject(UsersService);
+  private authSub = new Subscription();
   private guestSub = new Subscription();
   private usersSub = new Subscription();
-
   currentUser: User | null | undefined = undefined;
   loggedIn: boolean = false;
   showHeaderDropdown: boolean = false;
-
   readonly MAIN_ROUTES = ['/summary', '/add_task', '/board', '/contacts'];
   readonly OTHER_ROUTES = ['/help', '/legal_notice', '/privacy_policy'];
 
+
+  /**
+   * Create instance of router and subscribe to authentication service
+   * @param router instance of Router
+   */
   constructor(private router: Router) {
-    this.subAuth();
+    this.authSub = this.subAuth();
   }
 
+
+  /**
+   * Unsubscribe
+   */
   ngOnDestroy(): void {
+    this.authSub.unsubscribe();
     this.guestSub.unsubscribe();
     this.usersSub.unsubscribe();
   }
 
-  subAuth() {
-    this.authService.user$.subscribe(() => {
+
+  /**
+   * Subscribe to authService.user$ to check log in state.
+   * If a registered user is logged in, call subUsersInit() method.
+   * If the user is logged in as guest, call subGuestLogOut() method.
+   * @returns 
+   */
+  subAuth(): Subscription {
+    return this.authService.user$.subscribe(() => {
       const uid = this.authService.getCurrentUid();
       if (uid) {
         this.currentUser = this.usersService.getUserByUid(uid);
@@ -50,30 +70,59 @@ export class AppComponent implements OnDestroy {
     });
   }
 
+
+  /**
+   * Subscribe to usersService.user$ to retrieve detailed user data from usersService/Firestore
+   * @param uid Firebase user ID 
+   * @returns subscription
+   */
   subUsersInit(uid: string): Subscription {
     return this.usersService.users$.subscribe(() => {
       this.currentUser = this.usersService.getUserByUid(uid)
     });
   }
 
+
+  /**
+   * Subscribe to authService.guestLogOut$ to enable reaction to logging out when logged in as guest.
+   * @returns subscription
+   */
   subGuestLogOut(): Subscription {
     return this.authService.guestLogOut$.subscribe(() => this.localLogOut());
   }
 
+
+  /**
+   * Fit log in state properties to logging out.
+   */
   localLogOut() {
     this.currentUser = null;
     this.loggedIn = false;
     this.guestSub.unsubscribe();
   }
 
+
+  /**
+   * Get current route from router
+   * @returns current URL
+   */
   getCurrentRoute(): string {
     return this.router.url;
   }
 
+
+  /**
+   * Check if the current route is not included in app/main and app/other component routes.
+   * @returns check result
+   */
   onLogInPage(): boolean {
     return !(this.MAIN_ROUTES.includes(this.getCurrentRoute()) || this.OTHER_ROUTES.includes(this.getCurrentRoute()));
   }
 
+
+  /**
+   * Close header dropdown menu by setting the corresponding property
+   */
   closeHeaderDropdown() {
     this.showHeaderDropdown = false;
   }
